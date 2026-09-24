7d97fb7a95f3f7e2c20fdac65a814d876ee6a693
wmsmith28 <325407110+wmsmith28@users.noreply.github.com>
2026-09-24 18:54:40 -0400

    Fix v2.1.4 desktop version

--- backend/version.py
+++ backend/version.py
@@ -1,4 +1,4 @@
-__version__ = "2.1.3"
+__version__ = "2.1.4"
 GITHUB_REPO = "WinstonBaker/grade-calculator"
 WINDOWS_ASSET = "GradeCaculatorWindowsInstaller.exe"
 MACOS_ASSET = "GradeCalculator-macOS.dmg"
